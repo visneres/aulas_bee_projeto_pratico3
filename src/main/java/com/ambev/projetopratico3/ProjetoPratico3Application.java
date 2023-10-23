ca5ae141c789b1cba4a59fff13128bb3231c8d2f
Emerson Fonseca <idd_emefonseca@uolinc.com>
2023-10-23 18:06:21 -0300

    Inicial

---
+++ src/main/java/com/ambev/projetopratico3/ProjetoPratico3Application.java
@@ -0,0 +1,13 @@
+package com.ambev.projetopratico3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPratico3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPratico3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambev/projetopratico3/ProjetoPratico3ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ambev.projetopratico3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPratico3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Rodando teste..");
+	}
+
+}
